24499f49af6116c7f60c7cbff5e181d12b77c2c7
Shawn N Rebello <shawn.nigel@gmail.com>
2019-12-02 01:06:36 +0530

    connecting to backend server

--- src/LoadBalance.hs
+++ src/LoadBalance.hs
@@ -4,8 +4,13 @@ import           Config
 import           Control.Concurrent
 import           Control.Monad
 import qualified Data.ByteString.Char8     as C
+import           LoadBalance.Channel
 import           Network.Socket
 import           Network.Socket.ByteString
+import           Control.Exception
+import           Control.Concurrent.STM
+import           Control.Concurrent.STM.TChan
+
 
 startLoadBalance :: Config -> IO ()
 startLoadBalance config = do
@@ -36,3 +41,20 @@ messageReader sock =
     if msg == ""
       then close sock
       else putStrLn $ "Got msg: " <> msg
+
+connectToServer :: Config -> String -> String -> IO (Socket)
+connectToServer config host port =  catch (do
+  let upstream = host <> ":" <> port
+  let hints = defaultHints { addrSocketType = Stream }
+  addrinfos <- getAddrInfo (Just hints) (Just host) (Just port)
+  let serveraddr = head addrinfos
+  server <- socket (addrFamily serveraddr) Stream defaultProtocol
+  connect server (addrAddress serveraddr)
+  putStrLn $ "connected to " <> upstream
+  return server)
+  (\(e::IOError) -> do
+    let upstream = host <> ":" <> port
+    putStrLn $ "reconnecting to " <> upstream
+    threadDelay 1000000
+    connectToServer config host port
+  )
