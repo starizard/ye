d5caa7f7ff4eb7f8050d637cc127f4b75da366d8
Shawn N Rebello <shawn.nigel@gmail.com>
2019-12-07 12:03:17 +0530

    Added Hashing  fn

--- src/LoadBalance/Strategies.hs
+++ src/LoadBalance/Strategies.hs
@@ -4,7 +4,12 @@ import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Exception
 import           Control.Monad
-import qualified Network.Socket         as NS
+import           Crypto.Hash
+import qualified Data.ByteString.Char8    as C
+import qualified Data.ByteString.Internal as B
+
+-- import qualified Data.ByteString.Lazy.Internal as BL
+import qualified Network.Socket           as NS
 
 data Strategies
   = ROUND_ROBIN
@@ -19,6 +24,10 @@ data RequestState =
 
 getBalancer :: Strategies -> IO (RequestState -> IO NS.Socket)
 getBalancer balancingStrategy = do
+  let hashedOp = hashIP "hello"
+  let modval = C.foldl (\acc x -> acc ++ x) ' ' $ C.pack hashedOp
+  -- a `shiftL` 8 .|. fromIntegral b
+  putStrLn $ "HASH" ++ hashedOp
   case balancingStrategy of
     ROUND_ROBIN -> do
       counter <- newTVarIO 0
@@ -33,3 +42,8 @@ roundRobinBalancer counter reqState = do
   return $ sockets !! idx
   where
     getIdx value max = value `mod` max
+
+hashIP :: String -> String
+hashIP plaintext =
+  B.unpackChars $
+  digestToHexByteString ((hash (B.packChars plaintext)) :: Digest MD5)
